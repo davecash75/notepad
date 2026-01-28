ffed5e7aaa366eeaf09c7a5c26f356d04d7afef3
Dave Cash <d.cash@ucl.ac.uk>
2026-01-28 13:44:28 +0000

    Handle exception when issues with DICOM files

--- data_import/import_dian.py
+++ data_import/import_dian.py
@@ -72,11 +72,16 @@ def transfer_session(df_transfer, scan_filter=[]):
                 label=session_data.subject_label)
             else:
                 xnat_dest_subject = dest_subjects[session_data.subject_label]
-            archive_session = xnat_dest_server.services.import_dir(
-                                dl_path, 
-                                project=dest_project, 
-                                subject=xnat_dest_subject,
-                                experiment=label)
+            try:
+                archive_session = xnat_dest_server.services.import_dir(
+                                    dl_path, 
+                                    project=dest_project, 
+                                    subject=xnat_dest_subject,
+                                    experiment=label)
+            except Exception as e:
+                print(f"Exception on upload for {label}. Skipping ")
+                print(type(e))
+                print(e)
             
 
 def main():
